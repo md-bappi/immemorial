4a0e1926343750817e520ff48bff5ae3ce3151a1
md-bappi <mdabuanasbappy555@gmail.com>
2023-11-22 20:22:48 +0600

    featured done

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,5 +1,43 @@
+import { useRef } from "react";
+import {
+  useGsapFeatureLeftShutterUnveil,
+  useGsapFeatureRightShutterUnveil,
+} from "../hooks/gsap";
+
 const Featured = () => {
-  return <div>Featured</div>;
+  const featureRef = useRef(null);
+  const featureLeftShutterRef = useRef(null);
+  const featureRightShutterRef = useRef(null);
+
+  useGsapFeatureLeftShutterUnveil(featureLeftShutterRef, featureRef);
+  useGsapFeatureRightShutterUnveil(featureRightShutterRef, featureRef);
+
+  return (
+    <section className="reatured wrapper" ref={featureRef}>
+      <h2 className="section-title">Featured</h2>
+      <div className="features">
+        <div className="feature-l">
+          <span className="feature-text">90's TELEPHONE</span>
+          <img
+            src="https://images.pexels.com/photos/2587464/pexels-photo-2587464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="90's TELEPHONE"
+          />
+          <span className="feature-sutter-l" ref={featureLeftShutterRef}></span>
+        </div>
+        <div className="feature-r">
+          <span className="feature-text">90's CASSETTE PLAYER</span>
+          <img
+            src="https://images.pexels.com/photos/6076410/pexels-photo-6076410.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="90's CASSETTE PLAYER"
+          />
+          <span
+            className="feature-sutter-r"
+            ref={featureRightShutterRef}
+          ></span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Featured;
